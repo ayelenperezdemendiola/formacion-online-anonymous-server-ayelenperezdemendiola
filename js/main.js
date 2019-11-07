a538aa28aceea2ccd85d807faea14af63d495548
ayelenperezdemendiola <a.perezdemendiola@gmail.com>
2019-11-07 15:38:26 +0100

    fixed color nav

--- js/main.js
+++ js/main.js
@@ -4,12 +4,23 @@ const navOpenButton = document.querySelector ('.nav-trigger');
 const navCloseButton = document.querySelector ('.nav-close');
 const navList = document.querySelector ('.nav');
 
+navList.classList.add('hide');
 
-function showNav (){
+function showNav(){
+    navList.classList.remove('hide');
     navList.classList.add('show');
+    navOpenButton.classList.add('hide');
+    navOpenButton.classList.remove('show');
 }
-function hideNav (){
+
+function hideNav(){
     navList.classList.remove ('show');
+    navList.classList.add('hide');
+    navOpenButton.classList.remove('hide');
+    navOpenButton.classList.add('show');
 }
+
 navOpenButton.addEventListener('click', showNav);
 navCloseButton.addEventListener('click', hideNav);
+
+//no entiendo el error que me marca la consola

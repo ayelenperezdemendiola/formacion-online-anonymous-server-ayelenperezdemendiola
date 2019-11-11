6f5a4bf7ffd4d4ac70cba4234808aee83f1dcf54
ayelenperezdemendiola <a.perezdemendiola@gmail.com>
2019-11-11 09:37:00 +0100

    details

--- js/main.js
+++ js/main.js
@@ -4,18 +4,17 @@ const navOpenButton = document.querySelector ('.nav-trigger');
 const navCloseButton = document.querySelector ('.nav-close');
 const navList = document.querySelector ('.nav');
 
-navList.classList.add('hide');
-
 function showNav(){
-    navList.classList.remove('hide');
     navList.classList.add('show');
+    navList.classList.remove('hideNav');
+
     navOpenButton.classList.add('hide');
     navOpenButton.classList.remove('show');
 }
 
 function hideNav(){
     navList.classList.remove ('show');
-    navList.classList.add('hide');
+    navList.classList.add('hideNav');
     navOpenButton.classList.remove('hide');
 }
 
